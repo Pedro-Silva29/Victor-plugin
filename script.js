cf8afd8315233a3123776481ef0025cdbf373687
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-IH8VEDN>
2021-01-11 09:23:16 -0300

    Página carregando(nenhuma alteração)

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+
+See example:
+
+http://vignette1.wikia.nocookie.net/ultradragonball/images/2/27/KH1.png/revision/latest?cb=20120808234613
+
+http://vignette1.wikia.nocookie.net/dragonball/images/9/94/KameHouseShuPilafMaiEp03.png/revision/latest?cb=20120531192909
+
+http://i.skyrock.net/1097/25091097/pics/3131809152_1_2_rl6mVxlZ.png
+
+Font:
+https://fonts.google.com/specimen/PT+Sans+Narrow
+
+*/
